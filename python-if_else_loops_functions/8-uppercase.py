f1b2a8db79d0128f67ae3f131aac5b64bdcf21f6
Holberton <holberton@Holbertons-iMac.local>
2023-05-31 15:28:04 -0500

    8-uppercase.py update

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for item in range(len(str)):
-        upper = ord(str(item) - 32) if 97 <= ord(item) <= 122 else item
-        print(upper, end='')
-    print()
+    uppercase_str = ""
+    for i in range(len(str)):
+        char = ord(str[i])
+        uppercase_char = chr(char - 32) if 97 <= char <= 122 else chr(char)
+        uppercase_str += uppercase_char
+    print(uppercase_str)
