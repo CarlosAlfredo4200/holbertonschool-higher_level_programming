ce988ae5acc404614ae35e820f61bf0e760c79f1
Holberton <holberton@Holbertons-iMac.local>
2023-06-01 13:11:33 -0500

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+def print_arguments(argv):
+    num_arguments = len(argv)
+    
+    if num_arguments == 0:
+        print("0 arguments.")
+        print(".")
+    else:
+        print(f"{num_arguments} argument(s):")
+        for i in range(num_arguments):
+            print(f"{i+1}: {argv[i]}")
+
+if __name__ == "__main__":
+    print_arguments(sys.argv[1:])
