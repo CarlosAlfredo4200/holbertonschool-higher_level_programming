fc1d51f0d99254ff52bdf61e948e5b8f9d670ec4
Holberton <holberton@Holbertons-iMac.local>
2023-06-01 13:20:53 -0500

    2-args.py update

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-
 def print_arguments(argv):
     num_arguments = len(argv)
 
     if num_arguments == 0:
         print("0 arguments.")
     else:
-        print(f"{num_arguments} argument(s):")
+        print("{} argument(s):".format(num_arguments))
         for i in range(num_arguments):
             print(f"{i+1}: {argv[i]}")
 
