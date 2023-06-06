282163fb981da6c95178537376d3d0dc682289e5
Holberton <holberton@Holbertons-iMac.local>
2023-06-06 09:17:48 -0500

    1-element_at.py

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for item in my_list:
+        if idx < 0 or idx > len(my_list):
+            return None
+        else:
+            data = my_list[idx]
+            return data
