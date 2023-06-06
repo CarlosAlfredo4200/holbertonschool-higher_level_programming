fbfe9c694f1d19f41ca99bc7e47de4c7b6c4ca29
Holberton <holberton@Holbertons-iMac.local>
2023-06-06 16:34:36 -0500

    6-print_sorted_dictionary.py update

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]): 
-        print("{} : {}".format(key, value))
+    if a_dictionary:
+        for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]): 
+            print("{} : {}".format(key, value))
+    else:
+        return None
