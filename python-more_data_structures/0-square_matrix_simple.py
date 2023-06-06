357573a20ab009ad0d166e7cd075909d10c598d8
Holberton <holberton@Holbertons-iMac.local>
2023-06-06 14:39:51 -0500

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda row: list(map(lambda num: num ** 2, row)), matrix))
+    result = []
+    for row in matrix:
+        square_row = []
+        for num in row:
+            square_row.append(num ** 2)
+        result.append(square_row)
+    return result
