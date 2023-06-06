caf16da5353ce4aca7aff00c84b1fa0ef46021e7
Holberton <holberton@Holbertons-iMac.local>
2023-06-06 12:52:08 -0500

    9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max = 0
+        for item in my_list:
+            if item > max:
+                max = item
+        return max
+    else:
+        return None
