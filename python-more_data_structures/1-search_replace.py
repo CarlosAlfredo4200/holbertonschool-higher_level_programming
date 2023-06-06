1be2449f9aaaa9b0836168acf19fd0794e3a8807
Holberton <holberton@Holbertons-iMac.local>
2023-06-06 15:33:36 -0500

    2-uniq_add.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     return list(map(lambda item: replace if item == search else item, my_list))
+
+
+# def search_replace(my_list, search, replace):
+#     newList = []
+#     for item in my_list:
+#         if item != search:
+#             newList.append(item)
+#         else:
+#             newList.append(replace)
+#     return newList
