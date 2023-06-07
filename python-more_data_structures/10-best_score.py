40eaa48880f7ce22b3e8f48b3e89fae3e6978c3b
Holberton <holberton@Holbertons-iMac.local>
2023-06-07 08:52:11 -0500

    11-multiply_list_map.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -15,3 +15,15 @@ def best_score(a_dictionary):
             best_keys.append(key)
 
     return best_keys[0]
+
+
+
+
+# def best_score(a_dictionary):
+#     if not a_dictionary:
+#         return None
+    
+#     values = list(map(lambda x: a_dictionary[x], a_dictionary))
+#     max_value = max(values)
+#     best_keys = list(filter(lambda x: a_dictionary[x] == max_value, a_dictionary))
+#     return best_keys[0]
