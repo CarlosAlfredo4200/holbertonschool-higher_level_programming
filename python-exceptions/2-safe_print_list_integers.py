32b0e559b9cda3b66d88d7e958c260cd6e58c96a
Holberton <holberton@Holbertons-iMac.local>
2023-06-07 16:29:18 -0500

    2-safe_print_list_integers.py new

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    if x > len(my_list):
-        raise IndexError("Index out of range")
-
     try:
         for i in range(x):
-            if type(my_list[i]) is int:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
+            if i < len(my_list):
+                if type(my_list[i]) is int:
+                    print("{:d}".format(my_list[i]), end="")
+                    count += 1
+            else:
+                raise IndexError("Index out of range")
     except IndexError:
         pass
     else:
