0b9f705b298f40f46eb5bb87bf702fb2599c3531
Holberton <holberton@Holbertons-iMac.local>
2023-06-07 08:58:12 -0500

    12-roman_to_int.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))
+
+
+# def multiply_list(my_list=[], number=0):
+#     multiplied_list = []
+#     for value in my_list:
+#         multiplied_list.append(value * number)
+#     return multiplied_list
