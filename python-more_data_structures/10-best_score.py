5e324a42a4290733efc0887aab5ff1370a8f75bc
Holberton <holberton@Holbertons-iMac.local>
2023-06-07 08:47:10 -0500

    10-best_score.py update

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,8 +2,16 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    
-    values = list(map(lambda x: a_dictionary[x], a_dictionary))
+
+    values = []
+    for key in a_dictionary:
+        values.append(a_dictionary[key])
+
     max_value = max(values)
-    best_keys = list(filter(lambda x: a_dictionary[x] == max_value, a_dictionary))
+
+    best_keys = []
+    for key in a_dictionary:
+        if a_dictionary[key] == max_value:
+            best_keys.append(key)
+
     return best_keys[0]
