f714761a8fd21d87d6ab140057957838e857d430
Holberton <holberton@Holbertons-iMac.local>
2023-06-08 09:44:17 -0500

    4-list_division.py update

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    divide = lambda x, y: x / y if y != 0 else 0
+    def division(x, y): return x / y if y != 0 else 0
 
-    result_list = []
-    error_messages = []  
+    resultList = []
 
-    for i in range(list_length):
+    for item in range(list_length):
         try:
-            element_1 = my_list_1[i]
-            element_2 = my_list_2[i]
+            dataList1 = my_list_1[item]
+            dataList2 = my_list_2[item]
 
-            if not isinstance(element_1, (int, float)) or not isinstance(element_2, (int, float)):
-                error_messages.append("wrong type")
-                result_list.append(0)
+            if not isinstance(dataList1, (int, float)) or not isinstance(dataList2, (int, float)):
+                print("wrong type")
+                resultList.append(0)
                 continue
 
             try:
-                result_list.append(divide(element_1, element_2))
+                resultList.append(division(dataList1, dataList2))
             except ZeroDivisionError:
-                error_messages.append("division by 0")
-                result_list.append(0)
+                print("division by 0")
+                resultList.append(0)
 
         except IndexError:
-            error_messages.append("out of range")
-            result_list.append(0)
+            print("out of range")
+            resultList.append(0)
 
-    # Imprimir los mensajes de error
-    for message in error_messages:
-        print(message)
-
-    return result_list
+    return resultList
