465cee249019e601a7453cd1401308bda193ff67
Holberton <holberton@Holbertons-iMac.local>
2023-06-08 09:59:33 -0500

    4-list_division.py correction lambda

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     def division(x, y): return x / y if y != 0 else 0
-
     resultList = []
 
     for item in range(list_length):
         try:
             dataList1 = my_list_1[item]
             dataList2 = my_list_2[item]
+            resultList.append(division(dataList1, dataList2))
+        except ZeroDivisionError:
+            print("division by 0")
+            resultList.append(0)
 
-            if not isinstance(dataList1, (int, float)) or not isinstance(dataList2, (int, float)):
-                print("wrong type")
-                resultList.append(0)
-                continue
-
-            try:
-                resultList.append(division(dataList1, dataList2))
-            except ZeroDivisionError:
-                print("division by 0")
-                resultList.append(0)
+        except TypeError:
+            print("wrong type")
+            resultList.append(0)
 
         except IndexError:
             print("out of range")
