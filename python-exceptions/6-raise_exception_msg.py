fc1fe93096e47334ecd4c8079c956aef3d82ae76
Holberton <holberton@Holbertons-iMac.local>
2023-06-08 11:19:57 -0500

    6-raise_exception_msg.py update

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    mensaje = "Este es un mensaje de error personalizado"
-    
-     
-    raise NameError(mensaje)
+    raise NameError(message)
