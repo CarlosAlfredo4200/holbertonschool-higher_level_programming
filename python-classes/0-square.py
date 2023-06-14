164bc67c3fc3ff4ede6e0595dee30bcaea7ead56
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-06-13 23:01:55 -0500

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+"""Square module.
+
+    side_length (float): The length of the sides of the square.
+"""
+
+
 class Square:
-    """
-    Represents a square.
-
-    Attributes:
-        side_length (float): The length of the sides of the square.
-    """
-
     """
         Initializes a new instance of the Square class.
         """
