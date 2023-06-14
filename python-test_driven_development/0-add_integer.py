c1fa8fd7e75070ea0e558789060ab2712729f57b
Holberton <holberton@Holbertons-iMac.local>
2023-06-14 16:21:30 -0500

    0-add_integer.py documentacion

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-"""Este módulo contiene una función para sumar dos números enteros o de punto flotante."""
+"""Este módulo contiene una función para sumar dos números enteros."""
+
 
 def add_integer(a, b=98):
     """Esta función suma dos números enteros o de punto flotante.
 
     Args:
         a (int o float): El primer número a sumar.
-        b (int o float, opcional): El segundo número a sumar. Por defecto es 98.
-
+        b (int o float, opcional): El segundo número a sumar. Por defecto es 98
     Returns:
         int: La suma de los dos números.
 
-    Raises:
-        TypeError: Si alguno de los argumentos no es un número entero o de punto flotante.
 
-    Ejemplos:
-        >>> add_integer(2, 3)
-        5
-
-        >>> add_integer(-5, 10)
-        5
-
-        >>> add_integer(0, 0)
-        0
-
-        >>> add_integer('a', 'b')
-        Traceback (most recent call last):
-            ...
-        TypeError: a must be an integer
     """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
