3f084049084179224a1662497b120863234b66f0
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-06-14 00:04:16 -0500

    stop

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Square module.
+
+This module contains a class Square that defines a square
+sets its size
+"""
+
+
+class Square():
+    """Define a square"""
+
+    def __init__(self, size=0):
+        """Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """returns the current square area """
+        return self.__size * self.__size
