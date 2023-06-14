83628233e0d7973165777202ad8cf1dcd542bff9
Holberton <holberton@Holbertons-iMac.local>
2023-06-14 10:25:15 -0500

    6-square.py update

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,44 +7,30 @@ sets its size
 
 
 class Square:
-    """Class defined for square generation.
-    Args:
-        size (int): length of one side of square
-        position (tuple) ((int), (int)): horizontal offset in spaces,
-        vertical offset in newlines
-    Attributes:
-        __size (int): length of one side of square
-        __position (tuple) ((int), (int)): horizontal offset in spaces,
-        vertical offset in newlines
-    """
+    """Define a square"""
+
     def __init__(self, size=0, position=(0, 0)):
-        # attribute assigment here engages setters defined below
+        """Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """__size getter, setter with same method name
-        Returns:
-            __size (int): length of one side, squared
-        """
+        """returns the current square area """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Args:
-            value (int): length of one side of square
-        Attributes:
-            __size (int): length of one side of square
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than 0
-        """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('size must be >= 0')
-        self.__size = value
+        else:
+            self.__size = value
 
     @property
     def position(self):
@@ -65,7 +51,7 @@ class Square:
         Raises:
             TypeError: if value is not a tuple of two positive ints
         """
-        if type(value) is not tuple:
+        if not isinstance(value, tuple):
             raise TypeError('position must be a tuple of 2 positive integers')
         if len(value) is not 2:
             raise TypeError('position must be a tuple of 2 positive integers')
@@ -76,32 +62,18 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Calulates area of square.
-        Attributes:
-            __size (int): length of one side of square
-        Returns:
-            area (int): length of one side, squared
-        """
         area = self.__size * self.__size
         return area
 
     def my_print(self):
-        """Prints text representation of square in hash chars,
-        horizontally and vertically offset by first and second int
-        in __position, respectively.
-        Attributes:
-            __size (int): length of one side of square
-            __position (tuple) ((int), (int)): horizontal offset in spaces,
-            vertical offset in newlines
-        """
         if self.__size is 0:
             print()
         else:
-            for v_offset in range(0, self.__position[1]):
+            for item in range(0, self.__position[1]):
                 print()
-            for row in range(0, self.__size):
-                for h_offset in range(0, self.__position[0]):
+            for i in range(0, self.__size):
+                for k in range(0, self.__position[0]):
                     print(" ", end="")
-                for col in range(0, self.__size):
+                for item in range(0, self.__size):
                     print("#", end="")
                 print()
