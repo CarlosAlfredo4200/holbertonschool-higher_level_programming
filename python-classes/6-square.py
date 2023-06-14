b25482f4a4589fcfc606469e20525caac055f973
Holberton <holberton@Holbertons-iMac.local>
2023-06-14 10:14:02 -0500

    6-square.py update

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -27,10 +27,6 @@ class Square():
     def size(self):
         return self.__size
 
-    @property
-    def position(self):
-        return self.__position
-
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -40,25 +36,31 @@ class Square():
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        return self.__position
+
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(num, int) and num >= 0 for num in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self._position = value
+        self.__position = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
+        """Print the square with the # character."""
         if self.__size == 0:
-            print('')
-        else:
-            for item in range(self.__position[1]):
-                print('')
+            print("")
+            return
 
-            for item in range(self.__size):
-                print(' ' * self.__position[0] + "#" * self.__size)
-
-
-
-
-
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
