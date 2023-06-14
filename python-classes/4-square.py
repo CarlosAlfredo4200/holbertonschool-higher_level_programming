8c2a8541333c9200f1065d3508568a9b094d1c57
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-06-14 01:53:02 -0500

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -15,12 +15,22 @@ class Square():
         Args:
             size (int): The size of the square.
         """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
+
         self.__size = size
 
     def area(self):
         """returns the current square area """
         return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
