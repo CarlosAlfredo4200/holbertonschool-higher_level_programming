7f4bb1e14370660a4299ae415867e28cbe28448d
Holberton <holberton@Holbertons-iMac.local>
2023-06-15 10:22:53 -0500

    2-matrix_divided.py

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    """ Check if matrix is a list of lists of integers or floats"""
+    if not all(isinstance(row, list) for row in matrix) or not all(
+        isinstance(num, (int, float)) for row in matrix for num in row
+    ):
+        raise TypeError(
+            ("matrix must be a matrix (list of lists) of integers/floats"))
+
+    """ Check if each row of the matrix has the same size """
+    if len(set(len(row) for row in matrix)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+     
+    """Validate div input"""
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    
+    """ Validate div value"""
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    # return [[round(elem / div, 2) for elem in item] for item in matrix]
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
