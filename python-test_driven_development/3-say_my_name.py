0aa7d80a33185e2e666da4ccbf5ede94635c64ea
Holberton <holberton@Holbertons-iMac.local>
2023-06-15 11:53:33 -0500

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,24 +6,7 @@ def say_my_name(first_name, last_name=""):
     """
     Prints the name provided by concatenating the first name and last name.
 
-    Args:
-        first_name (str): The first name.
-        last_name (str, optional): The last name. Defaults to "".
 
-    Raises:
-        TypeError: If `first_name` or `last_name` is not a string.
-
-    Examples:
-        >>> say_my_name("John", "Doe")
-        My name is John Doe
-
-        >>> say_my_name("Jane")
-        My name is Jane
-
-        >>> say_my_name(123)
-        Traceback (most recent call last):
-        ...
-        TypeError: first_name must be a string
     """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
