6873774c107f39554d75c9cff21bbea967218d81
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-06-15 17:56:42 -0500

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,15 +3,15 @@
 
 
 def text_indentation(text):
-    """Prints text add two newlines
+    """Prints text with added two newlines
     after each of these characters {'.', '?', ':'}.
     """
-    if not isinstance(text, str):
-        raise TypeError('text must be a string')
 
-    punctuation_marks = ['.', '?', ':']
-    lines = text.splitlines()
-    for line in lines:
-        for mark in punctuation_marks:
-            line = line.replace(mark, mark + "\n\n")
-        print(line.strip())
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for caracters in ".:?":
+        text = (caracters + "\n\n").join(
+            [line.strip(" ") for line in text.split(caracters)])
+
+    print("{}".format(text), end="")
