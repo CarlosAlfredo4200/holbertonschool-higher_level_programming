3928bb634392756b703312de627e353b0d0fa56c
Holberton <holberton@Holbertons-iMac.local>
2023-06-16 10:08:46 -0500

    6-max_integer.py update

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,10 +5,13 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_len(self):
-         
         arr = [1, 2, 3, 4]
         self.assertEqual(max_integer(arr), 4)
 
+    def test_string(self):
+        list = [1,'hello']
+        with self.assertRaises(TypeError):
+            max_integer(list)
     
 if __name__ == '__main__':
     unittest.main()
