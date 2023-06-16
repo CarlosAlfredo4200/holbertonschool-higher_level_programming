76c5a906ce0509d25452f5db1d87d9e5debb7f08
Holberton <holberton@Holbertons-iMac.local>
2023-06-16 10:22:05 -0500

    6-max_integer.py update

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,21 +10,23 @@ class TestMaxInteger(unittest.TestCase):
     Test a function max_integer()
     """
 
-    def test_len(self):
-        list = [1, 2, 3, 4]
-        self.assertEqual(max_integer(list), 4)
+    def test_upper(self):
+        self.assertEqual(max_integer([6, 7, 8, 9]), 9)
 
-    def test_string(self):
-        list = [1, 'hello']
-        with self.assertRaises(TypeError):
-            max_integer(list)
+    def test_none(self):
+        self.assertEqual(max_integer([]), None)
 
-    def test_empty_list(self):
-        list = []
-        self.assertEqual(max_integer(list), None)
+    def test_one(self):
+        self.assertEqual(max_integer([2]), 2)
 
-    def test_void_arg(self):
-        self.assertEqual(max_integer(), None)
+    def test_one_neg(self):
+        self.assertEqual(max_integer([-10]), -10)
+
+    def test_neg(self):
+        self.assertEqual(max_integer([1, -2, -3, -4]), 1)
+
+    def test_middle(self):
+        self.assertEqual(max_integer([1, 3, 8, 2, 6]), 8)
 
 
 if __name__ == '__main__':
