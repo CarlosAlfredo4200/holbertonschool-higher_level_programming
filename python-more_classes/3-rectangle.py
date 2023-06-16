baf80a58b31865f06971612c4eccd15c9536c766
Holberton <holberton@Holbertons-iMac.local>
2023-06-16 13:21:43 -0500

    3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -53,10 +53,9 @@ class Rectangle:
             return 0
         else:
             return (self.__width * 2) + (self.__height * 2)
-        
-        
+
     def __str__(self):
-        """Sets the print behavior of the Rectangle object."""
+
         rectangle = ""
 
         if self.__width > 0 and self.__height > 0:
