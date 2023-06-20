5d02247fb2545a0228bc43fcbb5da742d8fbf319
Holberton <holberton@Holbertons-iMac.local>
2023-06-20 09:55:35 -0500

    1-main.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,11 +4,20 @@
 
 class MyList(list):
     """
-        Prints the list in sorted order (ascending sort).
-        Returns:
-        list: newList ascending sort
+    Prints the list in sorted order (ascending sort).
+    Returns:
+    list: newList ascending sort
+    """
 
-        """
+    def __init__(self, *args, **kwargs):
+        if args and args[0] is None:
+            raise TypeError("'None' object is not iterable")
+        super().__init__(*args, **kwargs)
+
+    def append(self, item):
+        if item is None:
+            raise TypeError("'None' object is not iterable")
+        super().append(item)
 
     def print_sorted(self):
         newList = sorted(self)
