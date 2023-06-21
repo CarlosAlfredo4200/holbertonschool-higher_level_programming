6394c4d925b5f78e3bd32c76f3c87a8ccfa31138
Holberton <holberton@Holbertons-iMac.local>
2023-06-21 10:13:12 -0500

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""a function that reads a text file (UTF8) and prints it to stdout:"""
+"""a function that writes a string to a text file (UTF8)"""
 
 
 def write_file(filename="", text=""):
-
+    """Writes a string to a text file (UTF-8) and returns the number of characters written."""
     with open(filename, "w", encoding="utf-8") as file:
         file.write(text)
         return len(text)
