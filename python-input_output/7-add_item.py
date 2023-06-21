8399cda9c3a8d5a415c0ef46302e0cef5ce4bbd6
Holberton <holberton@Holbertons-iMac.local>
2023-06-21 13:36:50 -0500

    7-add_item.py update 3

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""data"""
 import sys
 import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -7,9 +7,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def main():
+    """data"""
     list_args = sys.argv[1:]
+
     if os.path.exists("add_item.json"):
-        list_args - load_from_json_file("add_item.json") + list_args
+        list_args = load_from_json_file("add_item.json") + list_args
 
     save_to_json_file(list_args, "add_item.json")
 
