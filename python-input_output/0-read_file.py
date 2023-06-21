ad721a94928238d2628e08b372b6a5c7c0c45a85
Holberton <holberton@Holbertons-iMac.local>
2023-06-21 09:59:46 -0500

    0-read_file.py update

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,5 @@ def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, "r", encoding="utf-8") as file:
 
-        print("{}".format(file.read()), end='\n')
+        print(file.read(), end='')
+        """ print("{}".format(file.read()), end='\n')"""
