ed4d3de407b1049218555534e23045be66943731
Holberton <holberton@Holbertons-iMac.local>
2023-06-21 12:43:38 -0500

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
+
 """
-    6-from_json_string.py
-    Function that writes an Object to \
-    a text file, using a JSON representation.
+Add arguments to a list, then save those arguments to a file
 """
+
 import sys
-from typing import List
+import os.path
+
+
+args = sys.argv[1:]
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def add_arguments_to_list(arguments: List[str]) -> List[str]:
-    try:
-        
-        existing_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        existing_list = []
+new = []
+if os.path.exists("./add_item.json"):
+    new = load_from_json_file("add_item.json")
 
-    
-    updated_list = existing_list + arguments
-
-    
-    save_to_json_file(updated_list, "add_item.json")
-
-    return updated_list
-
-if __name__ == "__main__":
-    
-    arguments = sys.argv[1:]
-    added_items = add_arguments_to_list(arguments)
-    print("Items added to the list:", added_items)
+save_to_json_file(new + args, "add_item.json")
