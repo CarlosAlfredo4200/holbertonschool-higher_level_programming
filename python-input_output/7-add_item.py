2b52e6142c2db59cced9a36c384379fdfb592245
Holberton <holberton@Holbertons-iMac.local>
2023-06-21 11:28:41 -0500

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""a script that adds all arguments to a Python list"""
+"""Add item script."""
 import sys
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-""" Load existing data from file if it exists"""
 try:
-    data = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    data = []
+    lst = load_from_json_file("add_item.json")
+except:
+    lst = []
 
-""" Add command-line arguments to the list"""
 argc = len(sys.argv)
 
 if argc > 1:
     for i in range(1, argc):
-        data.append(sys.argv[i])
+        lst.append(sys.argv[i])
 
-""" Save the updated data to the file"""
-save_to_json_file(data, "add_item.json")
+save_to_json_file(lst, "add_item.json")
