d58dc4092801ea1b9bbb66addf8f52fdaae751ef
Holberton <holberton@Holbertons-iMac.local>
2023-06-21 13:33:57 -0500

    7-add_item.py update 3

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 
 import sys
-from typing import List
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_arguments_to_list(arguments: List[str]) -> List[str]:
-    try:
+def main():
+    list_args = sys.argv[1:]
+    if os.path.exists("add_item.json"):
+        list_args - load_from_json_file("add_item.json") + list_args
 
-        existing_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        existing_list = []
-
-    updated_list = existing_list + arguments
-
-    save_to_json_file(updated_list, "add_item.json")
-
-    return updated_list
+    save_to_json_file(list_args, "add_item.json")
 
 
 if __name__ == "__main__":
-
-    arguments = sys.argv[1:]
-    added_items = add_arguments_to_list(arguments)
-    print("Items added to the list:", added_items)
+    main()
