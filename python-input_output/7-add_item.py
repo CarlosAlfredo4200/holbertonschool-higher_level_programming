a8edc166b6d41d56330e755825af84b3d26e522c
Holberton <holberton@Holbertons-iMac.local>
2023-06-21 11:35:27 -0500

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
-"""Add item script."""
+"""
+    add_item.py
+    A script that adds all arguments to a Python list and saves them to a file.
+"""
 import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    lst = load_from_json_file("add_item.json")
-except:
-    lst = []
+def create_file_list():
+    """Adds all arguments to a Python list and saves them to a file."""
+    obj = load_from_json_file("add_item.json")
 
-argc = len(sys.argv)
+    for i in range(1, len(sys.argv)):
+        obj.append(str(sys.argv[i]))
 
-if argc > 1:
-    for i in range(1, argc):
-        lst.append(sys.argv[i])
+    save_to_json_file(obj, "add_item.json")
 
-save_to_json_file(lst, "add_item.json")
+
+if len(sys.argv) < 2:
+    try:
+        f = open("add_item.json")
+        f.close()
+    except IOError:
+        save_to_json_file([], "add_item.json")
+else:
+    try:
+        f = open("add_item.json")
+        f.close()
+        create_file_list()
+    except IOError:
+        save_to_json_file([], "add_item.json")
+        create_file_list()
