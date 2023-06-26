3265c229c8064c0c0cda5225219d8138994a7607
Holberton <holberton@Holbertons-iMac.local>
2023-06-26 13:06:03 -0500

    x

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
+"""class """
+
 
 class Base:
     """
-    This is the base class that manages the id attribute 
+    This is the base class that manages the id attribute
     for all other classes in the project.
     """
-
     __nb_objects = 0
 
     def __init__(self, id=None):
         """
         Initializes an instance of the Base class.
 
-        Args:
-            id (int): The ID value for the instance. If not provided, 
-            a unique ID will be assigned.
-
-        Attributes:
-            id (int): The ID value assigned to the instance.
         """
         if id is not None:
             self.id = id
