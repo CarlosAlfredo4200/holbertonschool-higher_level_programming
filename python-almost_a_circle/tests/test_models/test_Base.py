32ea977bef13762b32ee3eec4e74ad48e8fe6994
Holberton <holberton@Holbertons-iMac.local>
2023-06-26 12:45:22 -0500

    t

--- python-almost_a_circle/tests/test_models/test_Base.py
+++ python-almost_a_circle/tests/test_models/test_Base.py
@@ -2,12 +2,20 @@
 
 import unittest
 from models.base import Base
+b = Base()
+b1 = Base()
 class TestBase(unittest.TestCase):
     
     
-    def testBase_default(self):
-        self.assertEqual(Base(None))
-        self.assertEqual(Base(), 1)
+    def testBase_default(self, b):
+        self.assertEqual(b.id, 1)
+        
+    def testBase_default(self, b):
+        self.assertEqual(b1.id(None), 1)
+        
+     
+         
+     
         
         
 if __name__ == '__main__':
