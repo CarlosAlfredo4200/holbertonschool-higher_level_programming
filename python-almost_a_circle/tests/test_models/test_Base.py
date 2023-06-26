6ff2db7b4f3a951833b19c723b3f514f9c638123
Holberton <holberton@Holbertons-iMac.local>
2023-06-26 11:46:00 -0500

    test base

--- python-almost_a_circle/tests/test_models/test_Base.py
+++ python-almost_a_circle/tests/test_models/test_Base.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+import unittest
+from models.base import Base
+class TestBase(unittest.TestCase):
+    
+    
+    def testBase_default(self):
+        self.assertEqual(Base(), 1)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
