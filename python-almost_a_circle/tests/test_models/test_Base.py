f5eeefa3d1a12f844d87d265e0bd53748f840536
Holberton <holberton@Holbertons-iMac.local>
2023-06-26 13:03:43 -0500

    tests

--- python-almost_a_circle/tests/test_models/test_Base.py
+++ python-almost_a_circle/tests/test_models/test_Base.py
@@ -5,12 +5,21 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
+    """
+    Test cases for the Base class.
+    """
 
     def testBase_default(self):
+        """
+        Test the default behavior of creating an instance of Base without providing an ID.
+        """
         b = Base()
         self.assertEqual(b.id, 1)
         
     def testBase_default1(self):
+        """
+        Test creating an instance of Base with None as the ID argument.
+        """
         b = Base(None)
         self.assertEqual(b.id, 1)
 
