1fdc9e796669550b3cb2cad3835ca1fb981ca0aa
Holberton <holberton@Holbertons-iMac.local>
2023-06-28 11:05:40 -0500

    x

--- python-almost_a_circle/tests/test_models/test_Base.py
+++ python-almost_a_circle/tests/test_models/test_Base.py
@@ -6,12 +6,12 @@ class TestBase(unittest.TestCase):
 
     def test_base_id_generation(self):
         base1 = Base()
-        base2 = Base()
-        base3 = Base(89)
         self.assertEqual(base1.id, 1)
+        
+        base2 = Base()
         self.assertEqual(base2.id, 2)
-        self.assertEqual(base3.id, 89)
-
+         
+    
     def test_base_to_json_string_none(self):
         json_string = Base.to_json_string(None)
         self.assertEqual(json_string, "[]")
