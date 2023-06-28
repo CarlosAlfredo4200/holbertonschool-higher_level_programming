2664565916aac219da32c81764fc04ca764a50cd
Holberton <holberton@Holbertons-iMac.local>
2023-06-28 14:16:11 -0500

    10 square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,49 +1,73 @@
 #!/usr/bin/python3
-"""class """
-
+""" Module that contains class Square,
+inheritance of class Rectangle
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    This class represents a Square and inherits from
-    the Rectangle class.
-    """
+    """ Class Rectangle """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes an instance of the Square class.
-        """
+        """ Initializes instances """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ str special method """
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
 
     @property
     def size(self):
-        """
-        Getter method for the size attribute
-        (which is the same as width or height).
-        """
+        """ Getter size """
         return self.width
 
     @size.setter
     def size(self, value):
-        """
-        Setter method for the size attribute
-        (which is the same as width or height).
-        """
+        """ Setter size """
         self.width = value
         self.height = value
 
     def __str__(self):
-        """
-        Returns a string representation of the Square.
-        """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        """ str special method """
+        str_rectangle = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.size)
 
-    def display(self):
-        """
-        Prints a visual representation of the Square using the '#' character.
-        """
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(' ' * self.x + '#' * self.width)
+        return str_rectangle + str_id + str_xy + str_size
+
+    def update(self, *args, **kwargs):
+        """ update method """
+        if args is not None and len(args) is not 0:
+            list_atr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns a dictionary with attributes """
+        list_atr = ['id', 'size', 'x', 'y']
+        dict_res = {}
+
+        for key in list_atr:
+            if key == 'size':
+                dict_res[key] = getattr(self, 'width')
+            else:
+                dict_res[key] = getattr(self, key)
+
+        return dict_res
