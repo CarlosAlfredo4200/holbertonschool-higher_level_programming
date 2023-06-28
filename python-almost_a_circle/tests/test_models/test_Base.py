7a9eeb632f7174c516c1be8d2949e63a0c6609f6
Holberton <holberton@Holbertons-iMac.local>
2023-06-28 11:16:19 -0500

    la cague

--- python-almost_a_circle/tests/test_models/test_Base.py
+++ python-almost_a_circle/tests/test_models/test_Base.py
@@ -2,15 +2,23 @@ import unittest
 from models.base import Base
 
 
+
 class TestBase(unittest.TestCase):
 
-    def test_base_id_generation(self):
-        base1 = Base()
-        self.assertEqual(base1.id, 1)
+    
+    def multiples_args(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
 
-        base2 = Base()
-        self.assertEqual(base2.id, 2)
+        b3 = Base()
+        self.assertEqual(b1.id, b2.id, b3.id - 2)
 
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
+
+        self.assertEqual(20, Base().id)
 
 if __name__ == '__main__':
     unittest.main()
