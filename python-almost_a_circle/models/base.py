d1322536c985784d76961e6fc95176829703e767
Holberton <holberton@Holbertons-iMac.local>
2023-06-28 13:52:22 -0500

    update base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """class """
 import json
 
+
 class Base:
     """
     This is the base class that manages the id attribute
@@ -25,13 +26,6 @@ class Base:
         """
         Returns the JSON string representation of list_dictionaries.
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
-    
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        import json
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
