335feac94d53a38f1813ce1045599b61f35e2798
Holberton <holberton@Holbertons-iMac.local>
2023-06-28 13:06:25 -0500

    delete

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,74 +1,73 @@
-# #!/usr/bin/python3
-
 # import unittest
 # from models.rectangle import Rectangle
-
-# import unittest
-# from models.rectangle import Rectangle
-
 
 # class TestRectangle(unittest.TestCase):
+#     def test_valid_width_height(self):
+#         rect = Rectangle(1, 2)
+#         self.assertEqual(rect.width, 1)
+#         self.assertEqual(rect.height, 2)
 
-#     def test_rectangle_creation(self):
-#         rectangle = Rectangle(1, 2)
-#         self.assertEqual(rectangle.width, 1)
-#         self.assertEqual(rectangle.height, 2)
-#         self.assertEqual(rectangle.x, 0)
-#         self.assertEqual(rectangle.y, 0)
-#         self.assertIsNone(rectangle.id)
+#     def test_valid_width_height_x(self):
+#         rect = Rectangle(1, 2, 3)
+#         self.assertEqual(rect.width, 1)
+#         self.assertEqual(rect.height, 2)
+#         self.assertEqual(rect.x, 3)
 
-#     # def test_rectangle_creation_with_parameters(self):
-#     #     rectangle = Rectangle(1, 2, x=3, y=4, id=5)
-#     #     self.assertEqual(rectangle.width, 1)
-#     #     self.assertEqual(rectangle.height, 2)
-#     #     self.assertEqual(rectangle.x, 3)
-#     #     self.assertEqual(rectangle.y, 4)
-#     #     self.assertEqual(rectangle.id, 5)
+#     def test_valid_width_height_x_y(self):
+#         rect = Rectangle(1, 2, 3, 4)
+#         self.assertEqual(rect.width, 1)
+#         self.assertEqual(rect.height, 2)
+#         self.assertEqual(rect.x, 3)
+#         self.assertEqual(rect.y, 4)
 
-#     # def test_rectangle_creation_with_invalid_width(self):
-#     #     with self.assertRaises(ValueError):
-#     #         Rectangle("1", 2)
+#     def test_invalid_width_type(self):
+#         with self.assertRaises(TypeError):
+#             Rectangle("1", 2)
 
-#     # def test_rectangle_creation_with_invalid_height(self):
-#     #     with self.assertRaises(ValueError):
-#     #         Rectangle(1, "2")
+#     def test_invalid_height_type(self):
+#         with self.assertRaises(TypeError):
+#             Rectangle(1, "2")
 
-#     # def test_rectangle_creation_with_invalid_x(self):
-#     #     with self.assertRaises(ValueError):
-#     #         Rectangle(1, 2, x="3")
+#     def test_invalid_x_type(self):
+#         with self.assertRaises(TypeError):
+#             Rectangle(1, 2, "3")
 
-#     # def test_rectangle_creation_with_invalid_y(self):
-#     #     with self.assertRaises(ValueError):
-#     #         Rectangle(1, 2, y="4")
+#     def test_invalid_y_type(self):
+#         with self.assertRaises(TypeError):
+#             Rectangle(1, 2, 3, "4")
 
-#     # def test_rectangle_creation_with_negative_width(self):
-#     #     with self.assertRaises(ValueError):
-#     #         Rectangle(-1, 2)
+#     def test_invalid_extra_argument(self):
+#         with self.assertRaises(TypeError):
+#             Rectangle(1, 2, 3, 4, 5)
 
-#     # def test_rectangle_creation_with_negative_height(self):
-#     #     with self.assertRaises(ValueError):
-#     #         Rectangle(1, -2)
+#     def test_invalid_width_value(self):
+#         with self.assertRaises(ValueError):
+#             Rectangle(-1, 2)
 
-#     # def test_rectangle_creation_with_zero_width(self):
-#     #     with self.assertRaises(ValueError):
-#     #         Rectangle(0, 2)
+#     def test_invalid_height_value(self):
+#         with self.assertRaises(ValueError):
+#             Rectangle(1, -2)
 
-#     # def test_rectangle_creation_with_zero_height(self):
-#     #     with self.assertRaises(ValueError):
-#     #         Rectangle(1, 0)
+#     def test_invalid_x_value(self):
+#         with self.assertRaises(ValueError):
+#             Rectangle(0, 2)
 
-#     # def test_rectangle_creation_with_negative_x(self):
-#     #     with self.assertRaises(ValueError):
-#     #         Rectangle(1, 2, x=-3)
+#     def test_invalid_y_value(self):
+#         with self.assertRaises(ValueError):
+#             Rectangle(1, 0)
 
-#     # def test_rectangle_creation_with_negative_y(self):
-#     #     with self.assertRaises(ValueError):
-#     #         Rectangle(1, 2, y=-4)
+#     def test_invalid_x_negative_value(self):
+#         with self.assertRaises(ValueError):
+#             Rectangle(1, 2, -3)
 
-#     # def test_rectangle_area(self):
-#     #     rectangle = Rectangle(3, 4)
-#     #     self.assertEqual(rectangle.area(), 12)
+#     def test_invalid_y_negative_value(self):
+#         with self.assertRaises(ValueError):
+#             Rectangle(1, 2, 3, -4)
 
+#     def test_area(self):
+#         rect = Rectangle(3, 4)
+#         self.assertEqual(rect.area(), 12)
 
 # if __name__ == '__main__':
 #     unittest.main()
+
