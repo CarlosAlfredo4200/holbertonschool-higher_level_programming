589a1bff64d7f60af00f02b1e2be726388cc61b2
Holberton <holberton@Holbertons-iMac.local>
2023-06-28 11:07:33 -0500

    x

--- python-almost_a_circle/tests/test_models/test_Base.py
+++ python-almost_a_circle/tests/test_models/test_Base.py
@@ -2,47 +2,16 @@
 import unittest
 from models.base import Base
 
+
 class TestBase(unittest.TestCase):
 
     def test_base_id_generation(self):
         base1 = Base()
         self.assertEqual(base1.id, 1)
-        
+
         base2 = Base()
         self.assertEqual(base2.id, 2)
-         
-    
-    def test_base_to_json_string_none(self):
-        json_string = Base.to_json_string(None)
-        self.assertEqual(json_string, "[]")
 
-    def test_base_to_json_string_empty_list(self):
-        json_string = Base.to_json_string([])
-        self.assertEqual(json_string, "[]")
-
-    def test_base_to_json_string_with_data(self):
-        json_string = Base.to_json_string([{'id': 12}])
-        self.assertEqual(json_string, '[{"id": 12}]')
-
-    def test_base_to_json_string_with_data_returning_string(self):
-        json_string = Base.to_json_string([{'id': 12}])
-        self.assertIsInstance(json_string, str)
-
-    def test_base_from_json_string_none(self):
-        result = Base.from_json_string(None)
-        self.assertEqual(result, [])
-
-    def test_base_from_json_string_empty_list(self):
-        result = Base.from_json_string("[]")
-        self.assertEqual(result, [])
-
-    def test_base_from_json_string_with_data(self):
-        result = Base.from_json_string('[{"id": 89}]')
-        self.assertEqual(result, [{'id': 89}])
-
-    def test_base_from_json_string_with_data_returning_list(self):
-        result = Base.from_json_string('[{"id": 89}]')
-        self.assertIsInstance(result, list)
 
 if __name__ == '__main__':
     unittest.main()
