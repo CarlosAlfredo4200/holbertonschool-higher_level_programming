6dda9963d7c8f028077df3d05f97f3f9179af301
Holberton <holberton@Holbertons-iMac.local>
2023-06-29 09:21:22 -0500

    def create(cls, **dictionary):

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -48,3 +48,16 @@ class Base:
 
         with open(filename, 'w') as file:
             file.write(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with all attributes already set.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
